ce84c181b6391eafc26ffb2364ca5efafd9f1780
denliedesign <customdenlie@gmail.com>
2025-08-10 11:52:49 -0500

    reorder staff

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -59,14 +59,15 @@
 
     <div class="container">
         <h1 class="text-center my-5">Staff</h1>
-        <div class="d-flex justify-content-center my-2"><img src="/images/Allyson.png" alt="staff headshot" class="img-fluid"></div>
-        <div class="d-flex justify-content-center my-2"><img src="/images/Elle.png" alt="staff headshot" class="img-fluid"></div>
-        <div class="d-flex justify-content-center my-2"><img src="/images/Elliott.png" alt="staff headshot" class="img-fluid"></div>
-        <div class="d-flex justify-content-center my-2"><img src="/images/Emily.png" alt="staff headshot" class="img-fluid"></div>
-        <div class="d-flex justify-content-center my-2"><img src="/images/Jasmine.png" alt="staff headshot" class="img-fluid"></div>
         <div class="d-flex justify-content-center my-2"><img src="/images/LC.png" alt="staff headshot" class="img-fluid"></div>
+        <div class="d-flex justify-content-center my-2"><img src="/images/Allyson.png" alt="staff headshot" class="img-fluid"></div>
+        <div class="d-flex justify-content-center my-2"><img src="/images/Emily.png" alt="staff headshot" class="img-fluid"></div>
+        <div class="d-flex justify-content-center my-2"><img src="/images/Elliott.png" alt="staff headshot" class="img-fluid"></div>
+        <div class="d-flex justify-content-center my-2"><img src="/images/Jasmine.png" alt="staff headshot" class="img-fluid"></div>
         <div class="d-flex justify-content-center my-2"><img src="/images/Shay.png" alt="staff headshot" class="img-fluid"></div>
-{{--        <div class="d-flex justify-content-center my-2"><img src="/images/staff-1.jpg" alt="staff headshot" class="img-fluid"></div>--}}
+        <div class="d-flex justify-content-center my-2"><img src="/images/Elle.png" alt="staff headshot" class="img-fluid"></div>
+
+        {{--        <div class="d-flex justify-content-center my-2"><img src="/images/staff-1.jpg" alt="staff headshot" class="img-fluid"></div>--}}
 {{--        <div class="d-flex justify-content-center my-2"><img src="/images/staff-2.jpg" alt="staff headshot" class="img-fluid"></div>--}}
 {{--        <div class="d-flex justify-content-center my-2"><img src="/images/staff-3.jpg" alt="staff headshot" class="img-fluid"></div>--}}
 {{--        <div class="d-flex justify-content-center my-2"><img src="/images/staff-4.jpg" alt="staff headshot" class="img-fluid"></div>--}}
