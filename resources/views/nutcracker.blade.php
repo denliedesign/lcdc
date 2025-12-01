9d9ecc8ddad03e7f13cea7c394fd73bc740b111e
denliedesign <customdenlie@gmail.com>
2025-12-01 09:22:32 -0600

    nutcracker with link

--- resources/views/nutcracker.blade.php
+++ resources/views/nutcracker.blade.php
@@ -8,8 +8,8 @@
     <div class="container">
         <h1 class="text-center mt-5">The Nutcracker & Land of Sweets Party</h1>
         <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2">
-            <div class="col"><img src="/images/2025-nutcracker.png" alt="winter graphic" class="img-fluid my-3 rounded"></div>
-            <div class="col"><img src="/images/2025-land-of-sweets.png" alt="winter graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><a href="https://28996.danceticketing.com/r/events" target="_blank"><img src="/images/2025-nutcracker.png" alt="winter graphic" class="img-fluid my-3 rounded"></a></div>
+            <div class="col"><a href="https://28996.danceticketing.com/r/events" target="_blank"><img src="/images/2025-land-of-sweets.png" alt="winter graphic" class="img-fluid my-3 rounded"></a></div>
         </div>
     </div>
 
